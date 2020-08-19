772385811adf450b71f17c6884af02c23396985d
oscardaum <odaum14@gmail.com>
2020-08-18 20:20:52 -0400

    Updates

---
+++ precache-manifest.e7fdd896facf7959d79536d58713d480.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9a58aa2879f3b1e29233e5601e3eef",
+    "url": "/me.oscardaum/index.html"
+  },
+  {
+    "revision": "72dea49fd6e81c2cf72c",
+    "url": "/me.oscardaum/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "60c8671605d328ee1b7b",
+    "url": "/me.oscardaum/static/js/2.edc2f1fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me.oscardaum/static/js/2.edc2f1fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72dea49fd6e81c2cf72c",
+    "url": "/me.oscardaum/static/js/main.3f817268.chunk.js"
+  },
+  {
+    "revision": "f505c95db04818e56750",
+    "url": "/me.oscardaum/static/js/runtime-main.01f2918f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/me.oscardaum/static/media/logo.5d5d9eef.svg"
+  }
+]);
